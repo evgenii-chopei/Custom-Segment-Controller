af953f4f306285f65e6199ba360b6b74fc815e1c
evgenii22 <bojenkaaaa@gmail.com>
2016-09-23 16:22:51 +0300

    Update ECSegmentController.m

--- ECSegmentController.m
+++ ECSegmentController.m
@@ -25,6 +25,10 @@
 @property	(nonatomic)			CGRect				selectionFrame;
 @property	(nonatomic)			SelectionFrameType  selectionType;
 
+@property   (strong,nonatomic)    NSMutableArray          *views;
+@property   (strong,nonatomic)    NSMutableArray          *changesViews;
+@property   (nonatomic) BOOL isLabels;
+
 @end
 @implementation ECSegmentController
 
@@ -53,8 +57,31 @@
 }
 	
 }
+-(void)setUpViews{
+    for (UIView *view in self.views){
+        [view removeFromSuperview];
+    }
+    self.views =[[ NSMutableArray alloc]init];
+    CGFloat postitionX = self.bounds.size.width/self.items.count;
+    
+    for (int i = 0 ; i<self.items.count;i++){
+        UIImageView *view = [[UIImageView alloc]initWithFrame:CGRectMake(postitionX*i, 0, postitionX, self.bounds.size.height)];
+        [view setContentMode:UIViewContentModeScaleAspectFit];
+        CGRect subviewFrame = CGRectMake(view.center.x, view.center.y, view.frame.size.width/2, view.frame.size.height/2) ;
+       
+        view.image = [(UIImageView*)self.items[i]image];
+        [view setFrame:subviewFrame];
+        [self addSubview:view];
+        [view setCenter:view.center];
+        [self.views addObject:view];
+        
+    }
+    
+}
+
 - (void)displaySelectedIndex{
-	[self.labels enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+    if (_isLabels){
+    [self.labels enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
 		UILabel	*label = obj;
 		label.textColor = self.nonActiveIndexTextColor;
 	}];
@@ -67,6 +94,26 @@
 		
 		self.thumbView.frame =frame;
 	}];
+    }else{
+        [self.views enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            [(UIImageView*)obj setImage:[(UIImageView*)self.items[idx]image]];
+            
+        }];
+        UIImageView * view = self.views[self.calculatedIndex];
+        
+       [view setImage:self.changesViews[self.calculatedIndex]];
+        
+        
+        [UIView animateWithDuration:_animaionDuration animations:^{
+            CGRect frame = _selectionFrame;
+            frame.origin.x = view.frame.origin.x;
+            
+            self.thumbView.frame =frame;
+        }];
+
+        
+    }
 	
 }
 
@@ -76,7 +123,14 @@
 	self.layer.cornerRadius= self.frame.size.height/2;
 	self.layer.borderColor = self.borderColor.CGColor;
 	self.layer.borderWidth=self.borderWidth;
-	[self setUpLabels];
+    if ([self.items.firstObject isKindOfClass:[NSString class]]){
+        [self setUpLabels];
+        _isLabels =YES;
+    }else{
+        [self setUpViews];
+        
+    }
+    
 	self.thumbView= [[UIView alloc]initWithFrame:_selectionFrame];
 	[self insertSubview:self.thumbView atIndex:0];
 
@@ -85,13 +139,15 @@
 }
 - (BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event{
 	CGPoint location = [touch locationInView:self];
-	
-	[_labels enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+    id object;
+    if (_isLabels)object=_labels;
+    else object = _views;
+    
+	[object enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
 		if (CGRectContainsPoint([obj frame], location)){
 			_calculatedIndex = idx;
 			
-			
-		}
+			}
 		
 	}];
 	if (self.calculatedIndex!=self.selectedIndex){
@@ -115,19 +171,31 @@
 	if (self.selectionType == SelectionCircle) self.thumbView.layer.cornerRadius= self.thumbView.frame.size.height/2;
 	
 	
-
-	CGFloat labelWidth = self.bounds.size.width/self.labels.count;
-	
+    if (_isLabels){
+        
+        CGFloat labelWidth = self.bounds.size.width/self.labels.count;
+        CGFloat labelHeight = self.bounds.size.height;
 	for (int i = 0 ; i<self.labels.count;i++){
 		UILabel	*label = self.labels[i];
 		CGFloat xPosition = i*labelWidth;
-		label.frame = CGRectMake(xPosition, 0, labelWidth, _selectionFrame.size.height);
+		label.frame = CGRectMake(xPosition, 0, labelWidth, labelHeight);
 	}
+    }else{
+        
+        CGFloat labelWidth = self.bounds.size.width/self.views.count;
+        CGFloat labelHeight = self.bounds.size.height;
+        for (int i = 0 ; i<self.views.count;i++){
+            UIView	*view = self.views[i];
+            CGFloat xPosition = i*labelWidth;
+            view.frame = CGRectMake(xPosition, 0, labelWidth, labelHeight);
+        }
+
+    }
 	[self displaySelectedIndex];
 	
 	
 }
--(void)willMoveToSuperview{
+-(void)willMoveToSuperview:(UIView *)newSuperview{
 [self setUpView];
 }
 
@@ -175,5 +243,8 @@
 		frame.size.height=4;
 	}
 	_selectionFrame = frame;
+}
+- (void)setChangesViews:(NSArray*)array {
+    _changesViews = array;
 }
 @end
